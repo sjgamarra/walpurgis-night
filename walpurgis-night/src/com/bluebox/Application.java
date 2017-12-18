9968f60a4c31458805208bdccb91e58bb710908e
sgamarra <sgamarra@D7380150.proviasnac.dom>
2017-12-18 18:52:58 -0500

    Board practice

---
+++ walpurgis-night/src/com/bluebox/Application.java
@@ -0,0 +1,38 @@
+package com.bluebox;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+//http://zetcode.com/tutorials/javagamestutorial/basics/
+@SuppressWarnings("serial")
+public class Application extends JFrame{
+
+	private Board board;
+
+	public Application() {
+
+		initUI();
+	}
+
+	private void initUI() {
+
+		add(new Board());
+		setSize(600, 400);
+		setTitle("Application");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Application ex = new Application();
+				ex.setVisible(true);
+			}
+		});
+	}
+
+}
